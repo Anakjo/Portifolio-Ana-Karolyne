43c3359c0941153976106edd9754fc9eafa61fd8
Ana Karolyne <anakjo2006@gmail.com>
2026-03-15 00:01:13 -0300

    finalizando o projeto. Adicionando os 3 projetos nos div e realizando testes finais.

--- script.js
+++ script.js
@@ -10,26 +10,32 @@ botaoEsc.addEventListener ('click', mudarTema);
 
 //botão do formulário
 
-const texto = document.getElementById ('texto');
+const nome = document.getElementById ('nome');
 const email = document.getElementById ('email');
-const area = document.getElementById ('area');
+const mensagem = document.getElementById ('mensagem');
 const enviar = document.getElementById ('enviar');
 const formulario = document.getElementById ('formulario');
 
+// Validação de campos obrigatórios antes do envio dos dados
 formulario.addEventListener ('submit', function(event) {
-    if (texto.value === '') {
+    if (nome.value.trim() === '') {
         event.preventDefault();
-        alert('Você esqueceu de digitar o seu nome!')
+        alert('Você esqueceu de digitar o seu nome!');
     }
-    else if (email.value === '') {
+    else if (email.value.trim() === '') {
         event.preventDefault();
-        alert('Você esqueceu de digitar o seu e-mail!')
+        alert('Você esqueceu de digitar o seu e-mail!');
     }
-    else if (area.value === '') {
+    else if (mensagem.value.trim() === '') {
         event.preventDefault();
-        alert('Você esqueceu de digitar a sua mensagem!!')
+        alert('Você esqueceu de digitar a sua mensagem!!');
     }
     else {
         alert('Formulário enviado com sucesso!!')
     }
+
+    // Limpa os campos após o sucesso
+        nome.value = '';
+        email.value = '';
+        mensagem.value = '';
 });
